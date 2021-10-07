4910bfa5b6e84eed384c49ffacf274d86d3a815d
suxinyu <n1076436@ntub.edu.tw>
2021-10-07 14:21:54 +0800

    Fuzzball test

---
+++ Line Bot/Fuzzball.js/test.js
@@ -0,0 +1,93 @@
+fuzz = require('fuzzball')//require 模組
+
+options = { full_process: false, wildcards: "我想知道" }
+
+function user_question(quest) {
+    Str_quest = String(quest)
+    // var cla_1 = classify_1(Str_quest)
+    switch (classify_1(Str_quest)) {
+        case '股價':
+            var classify2 = classify_stock(quest, 1)
+            break;
+        case '新聞':
+            var classify2 = classify_news(quest, 1);
+            break;
+        case '理財':
+            var classify2 = '理財'
+            break;
+        case '財報':
+            var classify2 = '財報'
+            break;
+        default:
+            var classify2 = '沒有符合的條件'
+    }
+    return classify2
+}
+
+function classify_1(quest) {
+    Str_quest = String(quest)
+    a = classify_stock(Str_quest, 0)
+    b = classify_news(Str_quest, 0)
+    c = fuzz.ratio("我想知道理財", Str_quest)
+    d = fuzz.ratio("我想知道財報", Str_quest)
+    let dic = { "股價": a, "新聞": b, "理財": c, "財報": d }//將分數製作成字典
+    let keyArr = Object.keys(dic)//將字典轉換成組數
+    let max = 0
+    let re_classify_1 = ""
+    for (let i = 0; i < 4; i++) {
+        if (dic[keyArr[i]] > max) {
+            max = dic[keyArr[i]]
+        }
+    }
+    // 取出字典最大值
+    for (let key in dic) {
+        if (dic[key] == max) {
+            re_classify_1 = key
+        }
+    }
+    // 比對最大值屬於哪個鍵
+    return (re_classify_1)
+}
+
+function classify_news(quest, turn) {
+    Str_quest = String(quest)
+    let choices = ["期貨新聞", "台股新聞", "外匯新聞", "房產新聞"]
+    results = fuzz.extract(Str_quest, choices)//fuzz.extract(query, choices, options);
+    let max = 0
+    let max_text = 0
+    for (let i = 0; i < choices.length; i++) {
+        if (results[i][1] > max) {
+            max = results[i][1]//比較最大值
+            max_text = results[i][0]//取出最大值的字元
+        }
+    }
+    if (turn == 0) {
+        return (max)
+    } else {
+        return (max_text)
+    }
+}
+
+function classify_stock(quest, turn) {
+    Str_quest = String(quest)
+    let choices = ["2317股價", "2330股價", "2377股價", "2379股價", "2383股價", "1101股價", "2002股價", "2603股價", "2610股價", "9957股價"]
+    results = fuzz.extract(Str_quest, choices)//fuzz.extract(query, choices, options);
+    let max = 0
+    let max_text = 0
+    for (let i = 0; i < 10; i++) {
+        if (results[i][1] > max) {
+            max = results[i][1]//比較最大值
+            max_text = results[i][0]//取出最大值的字元
+        }
+    }
+    if (turn == 0) {
+        return (max)
+    } else {
+        return (max_text)
+    }
+}
+
+// let choices = ["2317股價", "2330股價", "2377股價", "2379股價", "2383股價", "1101股價", "2002股價", "2603股價", "2610股價", "9957股價","期貨新聞", "台股新聞", "外匯新聞", "房產新聞"]
+// console.log(fuzz.extract("我想知道最新台股新聞", choices))
+
+console.log(user_question("我想知道2377股價"))
