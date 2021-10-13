5c55a4f329c555f9d8453879c991e2b276717d3c
suxinyu <n1076436@ntub.edu.tw>
2021-10-13 21:08:25 +0800

    Fuzzball update

--- Line Bot/Fuzzball.js/fuzzball.js
+++ Line Bot/Fuzzball.js/fuzzball.js
@@ -6,17 +6,17 @@ module.exports.user_question = function user_question(quest) {
     Str_quest = String(quest)
     // var cla_1 = classify_1(Str_quest)
     switch (classify_1(Str_quest)) {
-        case '股價':
-            var classify2 = classify_stock(quest)
+        case '交易資料':
+            var classify2 = classify_stock(quest, 1)
             break;
-        case '新聞':
-            var classify2 = classify_news(quest);
+        case '查詢新聞':
+            var classify2 = classify_news(quest, 1);
             break;
-        case '理財':
-            var classify2 = '理財'
+        case '基本知識':
+            var classify2 = classify_kno(quest, 1)
             break;
-        case '財報':
-            var classify2 = '財報'
+        case '公司資料':
+            var classify2 = classify_com(quest, 1)
             break;
         default:
             var classify2 = '沒有符合的條件'
@@ -26,15 +26,15 @@ module.exports.user_question = function user_question(quest) {
 
 function classify_1(quest) {
     Str_quest = String(quest)
-    a = fuzz.ratio("我想知道股價", Str_quest)
-    b = fuzz.ratio("我想知道新聞", Str_quest)
-    c = fuzz.ratio("我想知道理財", Str_quest)
-    d = fuzz.ratio("我想知道財報", Str_quest)
-    let dic = { "股價": a, "新聞": b, "理財": c, "財報": d }//將分數製作成字典
+    a = classify_stock(Str_quest, 0)
+    b = classify_news(Str_quest, 0)
+    c = classify_kno(Str_quest, 0)
+    d = classify_com(Str_quest, 0)
+    let dic = { "交易資料": a, "查詢新聞": b, "基本知識": c, "公司資料": d }//將分數製作成字典
     let keyArr = Object.keys(dic)//將字典轉換成組數
     let max = 0
     let re_classify_1 = ""
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < keyArr.length; i++) {
         if (dic[keyArr[i]] > max) {
             max = dic[keyArr[i]]
         }
@@ -49,36 +49,83 @@ function classify_1(quest) {
     return (re_classify_1)
 }
 
-function classify_news(quest) {
+function classify_news(quest, turn) {
     Str_quest = String(quest)
-    let choices = ["期貨新聞", "台股新聞", "外匯新聞", "房產新聞"]
+    let choices = ["期貨新聞", "台股新聞", "外匯新聞", "房產新聞", "最近的新聞", "股票的正面消息"]
     results = fuzz.extract(Str_quest, choices)//fuzz.extract(query, choices, options);
     let max = 0
     let max_text = 0
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < choices.length; i++) {
         if (results[i][1] > max) {
             max = results[i][1]//比較最大值
             max_text = results[i][0]//取出最大值的字元
         }
     }
-    return (max_text)
+    if (turn == 0) {
+        return (max)
+    } else {
+        return (max_text)
+    }
 }
 
-function classify_stock(quest) {
+function classify_stock(quest, turn) {
     Str_quest = String(quest)
-    let choices = ["2317股價", "2330股價", "2377股價", "2379股價", "2383股價", "1101股價", "2002股價", "2603股價", "2610股價", "9957股價"]
+    let choices = ["台股走勢圖", "2317股價", "台積電股價", "2330股價", "2377股價", "2379股價", "2383股價", "1101股價", "2002股價", "2603股價", "2610股價", "9957股價"]
     results = fuzz.extract(Str_quest, choices)//fuzz.extract(query, choices, options);
     let max = 0
     let max_text = 0
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < choices.length; i++) {
         if (results[i][1] > max) {
             max = results[i][1]//比較最大值
             max_text = results[i][0]//取出最大值的字元
         }
     }
-    return (max_text)
+    if (turn == 0) {
+        return (max)
+    } else {
+        return (max_text)
+    }
 }
 
+function classify_kno(quest, turn) {
+    Str_quest = String(quest)
+    let choices = ["什麼是股票", "什麼是期貨", "什麼是債券", "什麼是ETF", "什麼是K線","什麼是KD線"]
+    results = fuzz.extract(Str_quest, choices)//fuzz.extract(query, choices, options);
+    let max = 0
+    let max_text = 0
+    for (let i = 0; i < choices.length; i++) {
+        if (results[i][1] > max) {
+            max = results[i][1]//比較最大值
+            max_text = results[i][0]//取出最大值的字元
+        }
+    }
+    if (turn == 0) {
+        return (max)
+    } else {
+        return (max_text)
+    }
+}
 
+function classify_com(quest, turn) {
+    Str_quest = String(quest)
+    let choices = ["台積電每股盈餘", "台積電經營能力", "台積電獲利能力"]
+    results = fuzz.extract(Str_quest, choices)//fuzz.extract(query, choices, options);
+    let max = 0
+    let max_text = 0
+    for (let i = 0; i < choices.length; i++) {
+        if (results[i][1] > max) {
+            max = results[i][1]//比較最大值
+            max_text = results[i][0]//取出最大值的字元
+        }
+    }
+    if (turn == 0) {
+        return (max)
+    } else {
+        return (max_text)
+    }
+}
 
+// let choices = ["2317股價", "2330股價", "2377股價", "2379股價", "2383股價", "1101股價", "2002股價", "2603股價", "2610股價", "9957股價","期貨新聞", "台股新聞", "外匯新聞", "房產新聞","關注清單","+","-"]
+// console.log(fuzz.extract("關注清單", choices))
 
+// console.log(user_question("我想了解台積電的每股盈餘"))
--- Line Bot/app.js
+++ Line Bot/app.js
@@ -3,7 +3,7 @@ const linebot = require('linebot');
 var follow_watch_two = require('./js/follow_watch_two');
 var follow_watch_one = require('./js/follow_watch_one');
 var flex = require("./js/flex");
-var fuzzball = require('./Fuzzball.js/fuzzball');
+var Fuzzball = require('./Fuzzball.js/Fuzzball');
 
 // 用於辨識Line Channel的資訊
 var bot = linebot({
@@ -14,57 +14,39 @@ var bot = linebot({
 
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
-  if (event.message.text == "1") {
-    event.reply(flex.text_message())
+  if (event.message.text == "關注清單") {
+    let t = ""
+    Promise.all([follow_watch_one.show_follow(event.source.userId)])
+      .then(([oneSecond]) => {
+        oneSecond.forEach(element => Promise.all([follow_watch_two.list(element)])
+          .then(([oneSecond]) => {
+            t = t + oneSecond
+          }));
+      })
+    setTimeout(() => {
+      // 三秒後回傳資料
+      event.reply(t).then(function (data) {
+        // 當訊息成功回傳後的處理
+      })
+    }, 2000);
+  } else if (event.message.text.substr(0, 1) == "+") {
+    let stock = event.message.text.substr(1, 4)
+    event.reply("將股票代號：" + stock + "加入關注清單").then(function (data) {
+      // 當訊息成功回傳後的處理
+      follow_watch_one.insertData_load(event.source.userId, stock)
+    })
+  } else if (event.message.text.substr(0, 1) == "-") {
+    let stock = event.message.text.substr(1, 4)
+    event.reply("將股票代號：" + stock + "從關注清單移除").then(function (data) {
+      // 當訊息成功回傳後的處理
+      follow_watch_one.remove_Data_load(event.source.userId, stock)
+    })
+  } else {
+    var message = Fuzzball.user_question(event.message.text)
+    event.reply(message).then(function (data) {
+      // 當訊息成功回傳後的處理
+    })
   }
-  if (event.message.text == "2") {
-    event.reply(flex.text_message1())
-  }
-  // var message = fuzzball.user_question(event.message.text)
-  // event.reply(message)
-  // if (event.message.text == "關注清單") {
-  //   let t = ""
-  //   Promise.all([follow_watch_one.show_follow(event.source.userId)])
-  //     .then(([oneSecond]) => {
-  //       oneSecond.forEach(element => Promise.all([follow_watch_two.list(element)])
-  //         .then(([oneSecond]) => {
-  //           t = t + oneSecond
-  //         }));
-  //     })
-  //   setTimeout(() => {
-  //     // 三秒後回傳資料
-  //     event.reply(t).then(function (data) {
-  //       // 當訊息成功回傳後的處理
-  //     })
-  //   }, 2000);
-  // }
-  // if (event.message.text == "安安") {
-  //   event.reply(flex.text_message()).then(function (data) {
-  //     // 當訊息成功回傳後的處理
-  //   })
-  // }
-  // if (event.message.text.substr(0,1) =="+"){
-  //   let stock = event.message.text.substr(1,4)
-  //   event.reply("將股票代號："+stock+"加入關注清單").then(function (data) {
-  //     // 當訊息成功回傳後的處理
-  //     follow_watch_one.insertData_load(event.source.userId,stock)
-  //   })
-  // }
-  // if (event.message.text.substr(0,1) =="-"){
-  //   let stock = event.message.text.substr(1,4)
-  //   event.reply("將股票代號："+stock+"從關注清單移除").then(function (data) {
-  //     // 當訊息成功回傳後的處理
-  //     follow_watch_one.remove_Data_load(event.source.userId,stock)
-  //   })
-  // }
-  // setTimeout(() => {
-  //   // 三秒後回傳資料
-  //   event.reply(message).then(function (data) {
-  //     // 當訊息成功回傳後的處理
-  //   })
-  // }, 3000);
-  // event.message.text是使用者傳給bot的訊息
-  // 準備要回傳的內容
 });
 
 bot.on('follow', function (event) {
