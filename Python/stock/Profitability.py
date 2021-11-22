3d5c1e8f7bea9327f1ee251af629ca66f6f80769
suxinyu <n1076436@ntub.edu.tw>
2021-11-23 00:10:53 +0800

    modify

--- Python/stock/Profitability.py
+++ Python/stock/Profitability.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import mysql.connector
 
-
-def Profitability_db(tuple_item):
+def Profit_ratio_bd(tuple_item):#利潤比例，項目:年、季度、股票代號、gpm(毛利率)、opm(營業利益率)、pbt(稅前淨利率)、pat(稅後淨利率)存入資料庫
     mydb = mysql.connector.connect(
       host="127.0.0.1",
       user="root",
@@ -13,7 +12,7 @@ def Profitability_db(tuple_item):
 
     mycursor = mydb.cursor()
     
-    sql = "INSERT INTO profitability (years,quarter,stock,gpm,roa,opm,roe,pbt,pb) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO test1 (years,quarter,stock,gpm,opm,pbt,pat) VALUES (%s,%s,%s,%s,%s,%s,%s)"
     val = tuple_item
     
     mycursor.execute(sql, val)
@@ -22,53 +21,177 @@ def Profitability_db(tuple_item):
     
     print(mycursor.rowcount, "record(s) affected")
 
-def Profitability(stock):
-    r = requests.get("https://tw.stock.yahoo.com/quote/"+str(stock)+".TW/profile") #將網頁資料GET下來
-    html = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
-    step1 = html.find_all(class_="table-grid row-fit-half")
+def Rate_of_return_bd(tuple_item):#報酬率，項目:年、季度、股票代號、roe(股東權益報酬率)、roa(資產報酬率)存入資料庫
+    mydb = mysql.connector.connect(
+      host="127.0.0.1",
+      user="root",
+      password="C23670424989",
+      database="ntub-line"
+    )
+
+    mycursor = mydb.cursor()
+    
+    sql = "INSERT INTO test2 (years,quarter,stock,roe,roa) VALUES (%s,%s,%s,%s,%s)"
+    val = tuple_item
+    
+    mycursor.execute(sql, val)
+    
+    mydb.commit()
+    
+    print(mycursor.rowcount, "record(s) affected")
+
+def profitability_bd(tuple_item):#報酬率，項目:年、季度、股票代號、roe(股東權益報酬率)、roa(資產報酬率)存入資料庫
+    mydb = mysql.connector.connect(
+      host="127.0.0.1",
+      user="root",
+      password="C23670424989",
+      database="ntub-line"
+    )
+
+    mycursor = mydb.cursor()
+    
+    sql = "INSERT INTO profitability (years,quarter,stock,gpm,opm,pbt,pat,roe,roa) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    val = tuple_item
+    
+    mycursor.execute(sql, val)
+    
+    mydb.commit()
+    
+    print(mycursor.rowcount, "record(s) affected")
+
+
+def Profit_ratio_old(stock):# 爬取歷年利潤比例 項目:年、季度、股票代號、gpm(毛利率)、opm(營業利益率)、pbt(稅前淨利率)、pat(稅後淨利率)
+    url="https://histock.tw/stock/"+str(stock)+"/%E5%88%A9%E6%BD%A4%E6%AF%94%E7%8E%87"
+    r=requests.get(url)
+    soup = BeautifulSoup(r.text,"html.parser")
+    soups = soup.find_all(class_="tb-stock tbBasic")
     count = 0
-    data_tuple = ()
-    data_tuple+=("2021",)
-    data_tuple+=("Q3",)
-    data_tuple+=(str(stock),)
-    for i in step1:
-        step2 = i.find_all(class_="grid-item item-span-6 break-mobile")
-        for i in step2:
-            step3 = i.find_all(class_="D(f) Ai(fs) H(100%) Fz(16px) Bxz(bb) BdB Bdc($bd-primary-divider) Lh(1.5)")
-            for i in step3:
-                if count==24:
-                    key = i.find("span",class_="As(st) Bxz(bb) Pstart(12px) Py(8px) Bgc(#f5f8fa) C($c-secondary-text) W(30%) Miw(u) Pend(12px) Mend(0)").get_text()
-                    value = i.find(class_="Py(8px) W(70%) Pstart(12px) Bxz(bb)").get_text()[:-1]
-                    data_tuple+=(value,)
-                if count==25:
-                    key = i.find("span",class_="As(st) Bxz(bb) Pstart(12px) Py(8px) Bgc(#f5f8fa) C($c-secondary-text) W(30%) Miw(u) Pend(12px) Mend(0)").get_text()
-                    value = i.find(class_="Py(8px) W(70%) Pstart(12px) Bxz(bb)").get_text()[:-1]
-                    data_tuple+=(value,)
-                if count==26:
-                    key = i.find("span",class_="As(st) Bxz(bb) Pstart(12px) Py(8px) Bgc(#f5f8fa) C($c-secondary-text) W(30%) Miw(u) Pend(12px) Mend(0)").get_text()
-                    value = i.find(class_="Py(8px) W(70%) Pstart(12px) Bxz(bb)").get_text()[:-1]
-                    data_tuple+=(value,)
-                if count==27:
-                    key = i.find("span",class_="As(st) Bxz(bb) Pstart(12px) Py(8px) Bgc(#f5f8fa) C($c-secondary-text) W(30%) Miw(u) Pend(12px) Mend(0)").get_text()
-                    value = i.find(class_="Py(8px) W(70%) Pstart(12px) Bxz(bb)").get_text()[:-1]
-                    data_tuple+=(value,)
-                if count==28:
-                    key = i.find("span",class_="As(st) Bxz(bb) Pstart(12px) Py(8px) Bgc(#f5f8fa) C($c-secondary-text) W(30%) Miw(u) Pend(12px) Mend(0)").get_text()
-                    value = i.find(class_="Py(8px) W(70%) Pstart(12px) Bxz(bb)").get_text()[:-1]
-                    data_tuple+=(value,)
-                if count==29:
-                    key = i.find("span",class_="As(st) Bxz(bb) Pstart(12px) Py(8px) Bgc(#f5f8fa) C($c-secondary-text) W(30%) Miw(u) Pend(12px) Mend(0)").get_text()
-                    value = i.find(class_="Py(8px) W(70%) Pstart(12px) Bxz(bb)").get_text()[:-2]
-                    data_tuple+=(value,)
-                count+=1 
-    Profitability_db(data_tuple)
+    for i in soups:
+        step1 = i.find_all('tr')
+        for i in step1:
+            array = []
+            step2 = i.find_all('td')
+            if count !=0:
+                array = []
+                count1 = 0
+                for i in step2:
+                    if count1 == 0:
+                        array.append(i.get_text()[:4])
+                        array.append(i.get_text()[-2:])
+                        array.append(stock)
+                        count1+=1
+                    else:
+                        array.append(i.get_text().replace("%",""))
+                        count1+=1
+                Profit_ratio_bd(tuple(array))
+            else:
+                count+=1
+
+def Rate_of_return_old(stock):# 爬取歷年報酬率，項目:年、季度、股票代號、roe(股東權益報酬率)、roa(資產報酬率)存入資料庫
+    url="https://histock.tw/stock/"+str(stock)+"/%E5%A0%B1%E9%85%AC%E7%8E%87"
+    r=requests.get(url)
+    soup = BeautifulSoup(r.text,"html.parser")
+    soups = soup.find_all(class_="tb-stock tbBasic")
+    count = 0
+    for i in soups:
+        step1 = i.find_all('tr')
+        for i in step1:
+            array = []
+            step2 = i.find_all('td')
+            if count !=0:
+                array = []
+                count1 = 0
+                for i in step2:
+                    if count1 == 0:
+                        array.append(i.get_text()[:4])
+                        array.append(i.get_text()[-2:])
+                        array.append(stock)
+                        count1+=1
+                    else:
+                        array.append(i.get_text().replace("%",""))
+                        count1+=1
+                Rate_of_return_bd(tuple(array))
+            else:
+                count+=1
+
+def Profit_ratio_new(stock):# 爬取最新利潤比例 項目:年、季度、股票代號、gpm(毛利率)、opm(營業利益率)、pbt(稅前淨利率)、pat(稅後淨利率)
+    url="https://histock.tw/stock/"+str(stock)+"/%E5%88%A9%E6%BD%A4%E6%AF%94%E7%8E%87"
+    r=requests.get(url)
+    soup = BeautifulSoup(r.text,"html.parser")
+    soups = soup.find_all(class_="tb-stock tbBasic")
+    count = 0
+    for i in soups:
+        step1 = i.find_all('tr',limit=2)
+        for i in step1:
+            array = []
+            step2 = i.find_all('td')
+            if count !=0:
+                array = []
+                count1 = 0
+                for i in step2:
+                    if count1 == 0:
+                        array.append(i.get_text()[:4])
+                        array.append(i.get_text()[-2:])
+                        array.append(stock)
+                        count1+=1
+                    else:
+                        array.append(i.get_text().replace("%",""))
+                        count1+=1
+                return(array)
+            else:
+                count+=1
+
+def Rate_of_return_new(stock):# 爬取最新報酬率、ROE(股東權益報酬率)、ROA(資產報酬率)
+    url="https://histock.tw/stock/"+str(stock)+"/%E5%A0%B1%E9%85%AC%E7%8E%87"
+    r=requests.get(url)
+    soup = BeautifulSoup(r.text,"html.parser")
+    soups = soup.find_all(class_="tb-stock tbBasic")
+    count = 0
+    for i in soups:
+        step1 = i.find_all('tr',limit=2)
+        for i in step1:
+            array = []
+            step2 = i.find_all('td')
+            if count !=0:
+                array = []
+                count1 = 0
+                for i in step2:
+                    if count1 == 0:
+                        count1+=1
+                    else:
+                        array.append(i.get_text().replace("%",""))
+                        count1+=1
+                return(array)
+            else:
+                count+=1
+
+def profitability_bd(tuple_item):#利潤比例，項目:年、季度、股票代號、gpm(毛利率)、opm(營業利益率)、pbt(稅前淨利率)、pat(稅後淨利率)存入資料庫
+    mydb = mysql.connector.connect(
+      host="127.0.0.1",
+      user="root",
+      password="C23670424989",
+      database="ntub-line"
+    )
+
+    mycursor = mydb.cursor()
     
-
-
-
-
-stock_array = [2317,2330,2377,2379,2383,1101,2015,2002,2603,2610]
- 
+    sql = "INSERT INTO test1 (years,quarter,stock,gpm,opm,pbt,pat) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+    val = tuple_item
     
-for i in stock_array:
-    Profitability(i)
+    mycursor.execute(sql, val)
+    
+    mydb.commit()
+    
+    print(mycursor.rowcount, "record(s) affected")
+
+def profitability():#爬取最新獲利能力資料並存入資料庫
+    stock_array = [2317,2330,2377,2379,2383,1101,2015,2002,2603,2610]
+    for i in stock_array:
+        array=[]
+        array.extend(Profit_ratio_new(i))
+        array.extend(Rate_of_return_new(i))
+        profitability_bd(tuple(array))
+
+
+profitability()
+
